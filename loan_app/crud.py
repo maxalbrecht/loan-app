f7e5ce021ee719614cc43bb9ed66bad0d96b2a01
maxalbrecht100 <max.albrecht100@gmail.com>
2024-04-15 16:38:08 -0400

    clean up commented out code

--- loan_app/crud.py
+++ loan_app/crud.py
@@ -32,12 +32,10 @@ def get_loan(db:Session, loan_id: int, skip: int = 0, limit: int = 100):
 
 
 def get_user_loans(db:Session, user_id: int, skip: int = 0, limit: int = 100):
-    #return db.query(models.Loan).filter(models.Loan.users.id == user_id).offset(skip).limit(limit).all()
     return db.query(models.Loan).filter(models.Loan.users.any(models.User.id == user_id)).offset(skip).limit(limit).all()
 
 
 def create_user_loan(db:Session, loan_create: schemas.LoanCreate):
-    #db_loan = models.Loan(**loan.dict())
     user_ids = loan_create.user_ids
     db_users = db.query(models.User).filter(models.User.id.in_(user_ids)).all()
     db_loan = models.Loan(
